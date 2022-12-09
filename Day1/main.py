ee31febc3ebec40f1ca98086298a281294ce3416
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2022-12-09 14:53:05 -0500

    Create function to return the elements of the input file.

--- Day1/main.py
+++ Day1/main.py
@@ -1,5 +1,14 @@
+def read_input_file() -> list[str]:
+    """Read the lines of the input file and remove newlines.
+
+    :return: A list containing the lines of the file as the elements.
+    """
+    with open('input.txt', 'r') as f:
+        return [ele.strip() for ele in f.readlines()]
+
+
 def main():
-    raise NotImplementedError
+    pass
 
 
 if __name__ == '__main__':
