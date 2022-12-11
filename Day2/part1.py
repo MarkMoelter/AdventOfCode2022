ff7d080fe84a910ad7238dfe616cb529b2b16d3b
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2022-12-10 19:26:19 -0500

    Move and rename rps_logic to game_logic.
    Place as a static method.

--- Day2/part1.py
+++ Day2/part1.py
@@ -7,6 +7,35 @@ class Part1:
     def __init__(self, games: list[tuple[str, str]]):
         self.games = games
 
+    @staticmethod
+    def game_logic(opponent, player) -> str:
+        """
+        Logic for rock, paper, scissors game.
+
+        :return: A string representation of the outcome of the game.
+        """
+        # win
+        rock_paper = opponent == Opponent.ROCK and player == Player.PAPER
+        scissor_rock = opponent == Opponent.SCISSORS and player == Player.ROCK
+        paper_scissors = opponent == Opponent.PAPER and player == Player.SCISSORS
+
+        # draw
+        both_rock = opponent == Opponent.ROCK and player == Player.ROCK
+        both_paper = opponent == Opponent.PAPER and player == Player.PAPER
+        both_scissors = opponent == Opponent.SCISSORS and player == Player.SCISSORS
+
+        # win
+        if rock_paper or scissor_rock or paper_scissors:
+            return 'W'
+
+        # draw
+        elif both_rock or both_paper or both_scissors:
+            return 'D'
+
+        # loss
+        else:
+            return 'L'
+
     def input_score(self) -> int:
         score = 0
 
@@ -36,35 +65,6 @@ class Part1:
 
     def total_score(self) -> int:
         return self.input_score() + self.outcome_score()
-
-    @staticmethod
-    def game_logic(opponent, player) -> str:
-        """
-        Logic for rock, paper, scissors game.
-
-        :return: A string representation of the outcome of the game.
-        """
-        # win
-        rock_paper = opponent == Opponent.ROCK and player == Player.PAPER
-        scissor_rock = opponent == Opponent.SCISSORS and player == Player.ROCK
-        paper_scissors = opponent == Opponent.PAPER and player == Player.SCISSORS
-
-        # draw
-        both_rock = opponent == Opponent.ROCK and player == Player.ROCK
-        both_paper = opponent == Opponent.PAPER and player == Player.PAPER
-        both_scissors = opponent == Opponent.SCISSORS and player == Player.SCISSORS
-
-        # win
-        if rock_paper or scissor_rock or paper_scissors:
-            return 'W'
-
-        # draw
-        elif both_rock or both_paper or both_scissors:
-            return 'D'
-
-        # loss
-        else:
-            return 'L'
 
 
 def separate_games() -> list[tuple[str, str]]:
--- Day2/part1_tests/test_part1.py
+++ Day2/part1_tests/test_part1.py
@@ -3,6 +3,37 @@ import unittest
 from Day2.part1 import Part1
 
 
+class TestGameLogic(unittest.TestCase):
+    def setUp(self) -> None:
+        self.draws = [
+            ('A', 'X'), ('B', 'Y'), ('C', 'Z')
+        ]
+
+        self.losses = [
+            ('A', 'Z'), ('B', 'X'), ('C', 'Y')
+        ]
+
+        self.wins = [
+            ('A', 'Y'), ('B', 'Z'), ('C', 'X')
+        ]
+
+        self.sol_win = Part1(self.wins)
+        self.sol_draw = Part1(self.draws)
+        self.sol_loss = Part1(self.losses)
+
+    def test_win_logic(self):
+        for opponent, player in self.wins:
+            self.assertEqual(self.sol_win.game_logic(opponent, player), 'W')
+
+    def test_draw_logic(self):
+        for opponent, player in self.draws:
+            self.assertEqual(self.sol_draw.game_logic(opponent, player), 'D')
+
+    def test_loss_logic(self):
+        for opponent, player in self.losses:
+            self.assertEqual(self.sol_loss.game_logic(opponent, player), 'L')
+
+
 class TestInputScore(unittest.TestCase):
     def setUp(self) -> None:
         self.draws = [
@@ -90,39 +121,6 @@ class TestTotalScore(unittest.TestCase):
 
     def test_returns_twenty_four_for_wins(self):
         self.assertEqual(self.sol_win.total_score(), 24)
-
-
-class TestGameLogic(unittest.TestCase):
-    def setUp(self) -> None:
-        self.draws = [
-            ('A', 'X'), ('B', 'Y'), ('C', 'Z')
-        ]
-
-        self.losses = [
-            ('A', 'Z'), ('B', 'X'), ('C', 'Y')
-        ]
-
-        self.wins = [
-            ('A', 'Y'), ('B', 'Z'), ('C', 'X')
-        ]
-
-        self.sol_win = Part1(self.wins)
-        self.sol_draw = Part1(self.draws)
-        self.sol_loss = Part1(self.losses)
-
-    def test_win_logic(self):
-        for opponent, player in self.wins:
-            self.assertEqual(self.sol_win.game_logic(opponent, player), 'W')
-
-    def test_draw_logic(self):
-        for opponent, player in self.draws:
-            self.assertEqual(self.sol_draw.game_logic(opponent, player), 'D')
-
-    def test_loss_logic(self):
-        for opponent, player in self.losses:
-            self.assertEqual(self.sol_loss.game_logic(opponent, player), 'L')
-
-
 
 
 if __name__ == '__main__':
