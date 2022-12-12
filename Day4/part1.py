50166d96f573b000c2854e331bb5d7a4e1013fcc
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2022-12-12 18:11:26 -0500

    Create process_input method and unittests.

--- Day4/part1.py
+++ Day4/part1.py
@@ -10,19 +10,35 @@ class CampCleanup:
     def __init__(self, pairs: list[str] = None):
         self.pairs = pairs
 
-    def process_input(self) -> list[tuple[set[int], set[int]]]:
+    def process_input(self) -> list[tuple[set[int], ...]]:
         """
         Convert the raw list of lines into something meaningful.
 
         :return: A list containing assignment pairs converted into sets.
         """
 
-        return []
+        processed = []
+
+        for pair in self.pairs:
+            # split into ranges
+            current = []
+            for range_ in pair.split(','):
+                # split ranges into upper and lower bounds
+                lower, upper = range_.split('-')
+                # convert bounds to the actual range values
+                set_val = {*range(int(lower), int(upper) + 1)}
+                current.append(set_val)
+
+            processed.append(tuple(current.copy()))
+            current.clear()
+
+        return processed
 
 
 def main():
     pairs = utils.read_input_file()
     part_1 = CampCleanup(pairs)
+    print(part_1.process_input())
 
 
 if __name__ == '__main__':
--- unittests/Day4/test_part1.py
+++ unittests/Day4/test_part1.py
@@ -5,10 +5,52 @@ from Day4 import CampCleanup
 
 class TestProcessInput(unittest.TestCase):
     def setUp(self) -> None:
-        pass
+        self.test_obj = CampCleanup(['2-5,3-3', '1-3,4-7'])
 
     def test_returns_list(self):
         self.assertIsInstance(
-            CampCleanup().process_input(),
-            list
+            self.test_obj.process_input(),
+            list,
+            'Should return list'
+        )
+
+    def test_list_not_empty(self):
+        self.assertTrue(
+            self.test_obj.process_input(),
+            'List should not be empty'
+        )
+
+    def test_returns_tuple(self):
+        self.assertIsInstance(
+            self.test_obj.process_input()[0],
+            tuple,
+            'Should return tuple'
+        )
+
+    def test_tuple_not_empty(self):
+        self.assertTrue(
+            self.test_obj.process_input()[0],
+            'Tuple should not be empty'
+        )
+
+    def test_returns_set(self):
+        self.assertIsInstance(
+            self.test_obj.process_input()[0][0],
+            set,
+            'Should return set'
+        )
+
+    def test_set_not_empty(self):
+        self.assertTrue(
+            self.test_obj.process_input()[0][0],
+            'Set should not be empty'
+        )
+
+    def test_returns_correct_output(self):
+        self.assertEqual(
+            self.test_obj.process_input(),
+            [
+                ({2, 3, 4, 5}, {3}),
+                ({1, 2, 3}, {4, 5, 6, 7})
+            ]
         )
