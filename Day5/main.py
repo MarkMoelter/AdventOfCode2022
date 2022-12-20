609766ee517737d8bf72e5bb3c2efa7c99ad1cc0
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2022-12-20 01:00:19 -0500

    Create function for part 1.

--- Day5/main.py
+++ Day5/main.py
@@ -1,13 +1,11 @@
 import logging
-from pprint import pprint
 
 import utils
 from Day5 import ParseInput, Part2, ShippingYard
 
 
-def main():
-    logging.basicConfig(level=logging.DEBUG)
-
+def part_1():
+    logging.info('Starting part 1')
     input_ = utils.read_input_file()
 
     parse = ParseInput(input_)
@@ -21,6 +19,14 @@ def main():
 
     # final image
     print(''.join(yard.last_in_each_column()))
+
+    logging.info('Finishing part 1')
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+
+    part_1()
 
     # part 2
     print(Part2())
