d4445c82a78d9f43a8b00af1ae222268a9d3213c
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2022-12-20 00:54:32 -0500

    Troubleshooting move_crates method.

--- Day5/Part1/command.py
+++ Day5/Part1/command.py
@@ -3,6 +3,6 @@ from dataclasses import dataclass
 
 @dataclass
 class Command:
-    blocks_to_move: int
-    initial_column: int
-    destination_column: int
+    num_crates: int
+    init_col: int
+    dest_col: int
--- Day5/Part1/shipping_yard.py
+++ Day5/Part1/shipping_yard.py
@@ -1,8 +1,10 @@
+import logging
+
 from .command import Command
 
 
 class ShippingYard:
-    def __init__(self, columns: dict = None):
+    def __init__(self, columns: dict[int, list[str]]):
         self.columns = columns
 
     def get_yard(self) -> dict:
@@ -12,14 +14,30 @@ class ShippingYard:
     def move_crates(self, cmd: Command) -> None:
         """Move blocks between columns. Alter the state of the columns."""
         # the column to change
-        init_column = self.columns[cmd.initial_column]
+        init_column = self.columns[cmd.init_col]
+        logging.debug('Initial column')
+        logging.debug(init_column)
 
-        # the crates to move from that column
-        crates = init_column[-cmd.blocks_to_move:]
+        # use list slicing to separate the crates into two lists
+        crates_to_move = init_column[-cmd.num_crates:]
+        updated_column = init_column[:-cmd.num_crates]
 
-        # remove the values from the column
-        self.columns[cmd.initial_column] = init_column[:-cmd.blocks_to_move]
+        logging.debug('List of crates to move')
+        logging.debug(crates_to_move)
 
         # add the crates to the destination column
-        for crate in crates:
-            self.columns[cmd.destination_column].append(crate)
+        self.columns[cmd.dest_col] += crates_to_move
+
+        # remove the crates from the column
+        self.columns[cmd.init_col] = updated_column
+
+        logging.debug('Updated column')
+        logging.debug(self.columns[cmd.init_col])
+
+    def last_in_each_column(self) -> list[str]:
+        """
+        Get the last value in each column.
+
+        :return: A list with the last value of each column.
+        """
+        return [col[-1] for col in self.columns.values()]
