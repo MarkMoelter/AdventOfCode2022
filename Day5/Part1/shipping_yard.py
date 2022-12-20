5c933ad275ccb8eb9a0d6218a0d1dadbf80deaed
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2022-12-20 00:54:32 -0500

    Troubleshooting move_crates method.

--- Day5/Part1/shipping_yard.py
+++ Day5/Part1/shipping_yard.py
@@ -1,25 +1,42 @@
+import logging
+
 from .command import Command
 
 
 class ShippingYard:
-    def __init__(self, columns: dict = None):
+    def __init__(self, columns: dict[int, list[str]]):
         self.columns = columns
 
-    def get_yard(self) -> dict:
-        """Print the columns using pretty print."""
+    def get_yard(self) -> dict[int, list[str]]:
+        """Return the columns object as a dictionary."""
         return self.columns
 
     def move_crates(self, cmd: Command) -> None:
         """Move blocks between columns. Alter the state of the columns."""
         # the column to change
-        init_column = self.columns[cmd.initial_column]
+        init_column = self.columns[cmd.init_col]
 
-        # the crates to move from that column
-        crates = init_column[-cmd.blocks_to_move:]
-
-        # remove the values from the column
-        self.columns[cmd.initial_column] = init_column[:-cmd.blocks_to_move]
+        # use list slicing to separate the crates into two lists
+        crates_to_move = init_column[-cmd.num_crates:]
+        updated_column = init_column[:-cmd.num_crates]
 
         # add the crates to the destination column
-        for crate in crates:
-            self.columns[cmd.destination_column].append(crate)
+        self.columns[cmd.dest_col] += crates_to_move
+
+        # remove the crates from the column
+        self.columns[cmd.init_col] = updated_column
+
+        logging.debug('Initial column')
+        logging.debug(init_column)
+        logging.debug('List of crates to move')
+        logging.debug(crates_to_move)
+        logging.debug('Updated column')
+        logging.debug(self.columns[cmd.init_col])
+
+    def last_in_each_column(self) -> list[str]:
+        """
+        Get the last value in each column.
+
+        :return: A list with the last value of each column.
+        """
+        return [col[-1] for col in self.columns.values()]
--- Day5/main.py
+++ Day5/main.py
@@ -1,3 +1,4 @@
+import logging
 from pprint import pprint
 
 import utils
@@ -5,21 +6,21 @@ from Day5 import ParseInput, Part2, ShippingYard
 
 
 def main():
+    logging.basicConfig(level=logging.DEBUG)
+
     input_ = utils.read_input_file()
 
     parse = ParseInput(input_)
     yard = ShippingYard(parse.column_setup())
 
-    # starting image
-    pprint(parse.column_setup())
-
     # part 1
     # move crates according to commands
-    for cmd in parse.parse_commands():
+    for i, cmd in enumerate(parse.parse_commands()):
+        logging.debug(i)
         yard.move_crates(cmd)
 
     # final image
-    pprint(yard.get_yard())
+    print(''.join(yard.last_in_each_column()))
 
     # part 2
     print(Part2())
