a79047ac417197cf1a4676dfa84151f58f297895
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2022-12-20 14:23:00 -0500

    Add input.txt and part1 function.

--- Days/Day6/main.py
+++ Days/Day6/main.py
@@ -3,10 +3,21 @@ import logging
 import utils
 
 
+def part_1():
+    # TODO: create a window function that searches for the first 4 unique characters in the input.
+    #  Will probably use sets and check for the length of the window set == 4.
+    #  Meaning there are 4 unique characters. Keep track of the index where it occurs.
+    input_ = utils.read_input_file()
+
+def part_2():
+    input_ = utils.read_input_file()
+
+
 def main():
     logging.basicConfig(level=logging.DEBUG)
 
-    input_ = utils.read_input_file()
+    part_1()
+    part_2()
 
 
 if __name__ == '__main__':
