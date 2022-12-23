c21b821de1921d9b74e641f0a1ef884cf74fb5df
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2022-12-23 13:48:52 -0500

    Add some unittests for create_file_system

--- unittests/Day7/test_create_file_system.py
+++ unittests/Day7/test_create_file_system.py
@@ -1,9 +1,29 @@
 import unittest
 
+from Days.Day7 import create_file_system, File
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+class TestCreateFileSystem(unittest.TestCase):
+    def setUp(self) -> None:
+        self.empty_input = []
+        self.single_file = ['1 abc.log']
+        self.invalid_file = ['12 7']
+
+    def test_returns_file(self):
+        self.assertIsInstance(
+            create_file_system(self.empty_input),
+            File
+        )
+
+    def test_root_has_files(self):
+        self.assertTrue(
+            create_file_system(self.single_file).contents
+        )
+
+    def test_root_does_not_add_invalid(self):
+        self.assertFalse(
+            create_file_system(self.invalid_file).contents
+        )
 
 
 if __name__ == '__main__':
--- unittests/Day7/test_file.py
+++ unittests/Day7/test_file.py
@@ -1,69 +1,16 @@
 import unittest
 
-from Days.Day7 import is_file
+from Days.Day7 import File
 
 
-class TestIsFile(unittest.TestCase):
-    def test_return_bool(self):
+class TestCalcSize(unittest.TestCase):
+    def setUp(self) -> None:
+        self.file = File('test', 100, False)
+
+    def test_returns_int(self):
         self.assertIsInstance(
-            is_file(''),
-            bool
-        )
-
-    def test_file_valid_wo_ext(self):
-        filename = '123 abs'
-        self.assertTrue(
-            is_file(filename),
-            f'"{filename}" is a valid file'
-        )
-
-    def test_valid_w_ext(self):
-        filename = '232 log.log'
-        self.assertTrue(
-            is_file(filename),
-            f'"{filename}" is a valid file'
-        )
-
-    def test_invalid_ext(self):
-        filename = '1 abd.sd'
-        self.assertFalse(
-            is_file(filename),
-            f'"{filename}" is not a valid file'
-        )
-
-    def test_invalid_name(self):
-        filename = '10378 7.asd'
-        self.assertFalse(
-            is_file(filename),
-            f'"{filename}" is not a valid file'
-        )
-
-    def test_neg_size(self):
-        filename = '-123 adg'
-        self.assertFalse(
-            is_file(filename),
-            f'"{filename}" is not a valid file'
-        )
-
-    def test_size_0(self):
-        filename = '0 gie.sdf'
-        self.assertFalse(
-            is_file(filename),
-            f'"{filename}" is not a valid file size'
-        )
-
-    def test_valid_with_0_in_size(self):
-        filename = '1201 tot.exc'
-        self.assertTrue(
-            is_file(filename),
-            f'"{filename}" is a valid file size'
-        )
-
-    def test_leading_0_in_size(self):
-        filename = '0000000430 abcd'
-        self.assertTrue(
-            is_file(filename),
-            f'"{filename}" is a valid file size'
+            self.file.calc_size(),
+            int
         )
 
 
