4a76b5b293dadb42a408f9713105ce9735311bb2
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2022-12-22 23:46:47 -0500

    Add files and subdirectories fields to Directory dataclass.

--- Days/Day7/Part1/data_structures.py
+++ Days/Day7/Part1/data_structures.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
@@ -10,3 +10,14 @@ class File:
 @dataclass
 class Directory:
     name: str
+    files: list[File] = field(default_factory=list)
+    sub_directories: list = field(default_factory=list)
+
+    def file_size(self) -> int:
+        """
+        Return the size of the files in the directory.
+        Does not include subdirectories.
+
+        :return: The total in bytes of all files in the directory.
+        """
+        return sum([file.size for file in self.files])
