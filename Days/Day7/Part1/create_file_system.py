d2e4a63eb6fa6a476df050fdfd5edc26e048c5b4
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2022-12-23 10:29:23 -0500

    Remove regex functions.
    Regex not needed in the example.

--- Days/Day7/Part1/create_file_system.py
+++ Days/Day7/Part1/create_file_system.py
@@ -1,27 +1,4 @@
-import re
-
 from .data_structures import File
-
-
-def is_command(potential_command: str) -> bool:
-    """
-    Check if a string is considered a command.
-
-    Checks for a '$' to start the string, a space, then a cd or ls
-     command, then an optional directory after the command.
-    Format: '$ cd/ls opt(directory)'
-
-    :param potential_command: The string to check.
-    :return: True if the string is a command, False otherwise.
-    """
-    regex_cmd = r'^\$\s(cd|ls)(\s[a-zA-Z/\.]*)?$'
-    return bool(re.search(regex_cmd, potential_command))
-
-
-def is_file(potential_file: str) -> bool:
-    """Check if a string is considered a file."""
-    regex_file = r'^[0]*[1-9]+\d*\s[a-zA-Z]+(\.[a-zA-Z]{3})?$'
-    return bool(re.search(regex_file, potential_file))
 
 
 def create_file_system(data_stream: list[str]) -> File:
