924617937df8c450562b9e526fe37ccf4b885300
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2022-12-24 00:54:04 -0500

    Add main.py boilerplate.

--- Days/Day8/main.py
+++ Days/Day8/main.py
@@ -1,9 +1,16 @@
 import logging
 
+import utils
+
+from Days.Day8 import Visibility
+
 
 def part_1():
     """Determine how many trees are visible from outside the grid."""
-    pass
+    forest = utils.read_input_file('test_input.txt')
+    vis = Visibility(forest)
+    print(vis.count_visible_trees())
+
 
 
 def part_2():
