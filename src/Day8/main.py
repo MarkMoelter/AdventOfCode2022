b6de703b1eff55c0db44c52b0c7f57130734eed4
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2022-12-29 22:03:18 -0500

    Setup main.py
    Add the details for part 1.

---
+++ src/Day8/main.py
@@ -0,0 +1,25 @@
+import logging
+
+import utils
+from src.Day8 import Visibility
+
+
+def part_1():
+    """Determine how many trees are visible from outside the grid."""
+    forest = utils.read_input_file('test_input.txt')
+    vis = Visibility(forest)
+    print(vis.count_visible_trees())
+
+
+def part_2():
+    pass
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    part_1()
+    part_2()
+
+
+if __name__ == '__main__':
+    main()
